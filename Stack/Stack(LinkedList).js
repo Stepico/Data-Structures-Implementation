4c40da048d0ab7f1af1a86ca56a75affcb0f51f8
Stepan Prokopyev <stepkapro2002@gmail.com>
2023-07-13 09:56:54 +0300

    Added explanations to the Stack(LinkedList)

--- Stack/Stack(LinkedList).js
+++ Stack/Stack(LinkedList).js
@@ -46,12 +46,45 @@ class Stack {
 
 const myStack = new Stack();
 console.log(myStack.push('google'));
+// Stack {
+//   top: Node { value: 'google', next: null },
+//   bottom: Node { value: 'google', next: null },
+//   length: 1
+// }
 console.log(myStack.push('udemy'));
+// Stack {
+//   top: Node { value: 'udemy', next: Node { value: 'google', next: null } },
+//   bottom: Node { value: 'google', next: null },
+//   length: 2
+// }
 console.log(myStack.peek())
+// Node { value: 'udemy', next: Node { value: 'google', next: null } }
 console.log(myStack.push('discord'));
+// Stack {
+//   top: Node {
+//     value: 'discord',
+//     next: Node { value: 'udemy', next: [Node] }
+//   },
+//   bottom: Node { value: 'google', next: null },
+//   length: 3
+// }
 console.log(myStack.pop())
+// Stack {
+//   top: Node { value: 'udemy', next: Node { value: 'google', next: null } },
+//   bottom: Node { value: 'google', next: null },
+//   length: 2
+// }
 console.log(myStack.isEmpty())
+// false
 console.log(myStack.pop())
+// Stack {
+//   top: Node { value: 'google', next: null },
+//   bottom: null,
+//   length: 1
+// }
 console.log(myStack.peek())
+// Node { value: 'google', next: null }
 console.log(myStack.pop())
+// Stack { top: null, bottom: null, length: 0 }
 console.log(myStack.isEmpty())
+// true
